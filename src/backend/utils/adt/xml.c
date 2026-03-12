cf07beb1e8f4e7ba66bf4b0b5e1429c8ee4115e7
jian he <jian.universality@gmail.com>
2026-03-12 09:36:05 +0800

    error safe for casting character to other types per pg_cast
    select castsource::regtype, casttarget::regtype, castfunc,
    castcontext,castmethod, pp.prosrc, pp.proname from pg_cast pc join pg_proc pp on
    pp.oid = pc.castfunc and pc.castfunc > 0
    and castsource::regtype ='character'::regtype
    order by castsource::regtype;
    
     castsource |    casttarget     | castfunc | castcontext | castmethod |   prosrc    | proname
    ------------+-------------------+----------+-------------+------------+-------------+---------
     character  | text              |      401 | i           | f          | rtrim1      | text
     character  | character varying |      401 | i           | f          | rtrim1      | text
     character  | "char"            |      944 | a           | f          | text_char   | char
     character  | name              |      409 | i           | f          | bpchar_name | name
     character  | xml               |     2896 | e           | f          | texttoxml   | xml
     character  | character         |      668 | i           | f          | bpchar      | bpchar
    (6 rows)
    
    only texttoxml, bpchar(PG_FUNCTION_ARGS) need take care of error handling.
    other functions already error safe.
    
    discussion: https://postgr.es/m/CADkLM=fv1JfY4Ufa-jcwwNbjQixNViskQ8jZu3Tz_p656i_4hQ@mail.gmail.com

--- src/backend/utils/adt/xml.c
+++ src/backend/utils/adt/xml.c
@@ -660,7 +660,7 @@ texttoxml(PG_FUNCTION_ARGS)
 {
 	text	   *data = PG_GETARG_TEXT_PP(0);
 
-	PG_RETURN_XML_P(xmlparse(data, xmloption, true));
+	PG_RETURN_XML_P(xmlparse(data, xmloption, true, fcinfo->context));
 }
 
 
@@ -1029,19 +1029,25 @@ xmlelement(XmlExpr *xexpr,
 
 
 xmltype *
-xmlparse(text *data, XmlOptionType xmloption_arg, bool preserve_whitespace)
+xmlparse(text *data, XmlOptionType xmloption_arg, bool preserve_whitespace, Node *escontext)
 {
 #ifdef USE_LIBXML
 	xmlDocPtr	doc;
 
 	doc = xml_parse(data, xmloption_arg, preserve_whitespace,
-					GetDatabaseEncoding(), NULL, NULL, NULL);
-	xmlFreeDoc(doc);
+					GetDatabaseEncoding(), NULL, NULL, escontext);
+	if (doc)
+		xmlFreeDoc(doc);
+
+	if (SOFT_ERROR_OCCURRED(escontext))
+		return NULL;
 
 	return (xmltype *) data;
 #else
-	NO_XML_SUPPORT();
-	return NULL;
+	ereturn(escontext, NULL,
+			errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			errmsg("unsupported XML feature"),
+			errdetail("This functionality requires the server to be built with libxml support."));
 #endif
 }
 
